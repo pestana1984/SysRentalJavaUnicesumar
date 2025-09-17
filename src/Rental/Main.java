22e6de06a4cd99cb22d47f9d52f63910b8b97c92
felipe.pestana <felipe.pestana@sitic.com.br>
2025-09-16 21:51:01 -0300

    Segundo dia da revisão

--- src/Rental/Main.java
+++ src/Rental/Main.java
@@ -1,29 +1,133 @@
 package Rental;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    static int Menu() {
-        System.out.println("Menu");
-        System.out.println("1 - Gerenciar Clientes");
-        System.out.println("2 - Gerenciar Veiculos");
-        System.out.println("3 - Gerenciar Locações");
-        System.out.println("4 - Sair");
-        System.out.print("Escolha uma opcao: ");
+    static Costumer CreateCostumer(){
+
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+
+        System.out.println("Informe o nome do cliente: ");
+        var name = sc.nextLine();
+        System.out.println("Informe o CPF do cliente: ");
+        var cpf = sc.nextLine();
+        System.out.println("Informe o numero do cartão de credito do cliente: ");
+        var credit =  sc.nextLine();
+        System.out.println("Informe o numero da habilitação: ");
+        var document = sc.nextLine();
+
+        return new Costumer(cpf, name, credit, document);
+    }
+
+    static void ShowCostumerList(ArrayList<Costumer> list){
+        if(list.isEmpty()){
+            System.out.println("Lista vazia");
+        }
+        else {
+            for(int i = 0; i < list.size(); i++){
+                System.out.println(list.get(i).getName());
+                System.out.println(list.get(i).getCpf());
+                System.out.println(list.get(i).getCreditCardNumber());
+                System.out.println(list.get(i).getLicenseNumber());
+            }
+        }
     }
 
     public static void main(String[] args) {
 
+        var costumers = new ArrayList<Costumer>();
+        var vehicles = new ArrayList<Vehicle>();
+        var rentals = new ArrayList<Rental>();
+
+        var optionsMain = new ArrayList<String>();
+        optionsMain.add("1 - Gerenciar Clientes");
+        optionsMain.add("2 - Gerenciar Veículos");
+        optionsMain.add("3 - Gerenciar Locações");
+        optionsMain.add("4 - Sair");
+
+        var mainMenu = new Menu(optionsMain, "Menu Principal");
+
+        var costumerMenuList = new ArrayList<String>();
+
+        costumerMenuList.add("1 - Cadastrar Cliente");
+        costumerMenuList.add("2 - Listar Todos os Cliente");
+        costumerMenuList.add("3 - Remover Cliente");
+        costumerMenuList.add("0 - Voltar ao Anterior");
+
+        var costumerMenu = new Menu(costumerMenuList, "Menu Clientes");
+
+        var vehicleOptions = new ArrayList<String>();
+        vehicleOptions.add("1 - Cadastrar Veiculo");
+        vehicleOptions.add("2 - Listar Todos os Veiculos");
+        vehicleOptions.add("3 - Remover Veiculo");
+        vehicleOptions.add("0 - Voltar ao Anterior");
+
+        var vehicleMenu = new Menu(vehicleOptions, "Menu Veiculos");
+
+        var rentalOptions = new ArrayList<String>();
+        rentalOptions.add("1 - Cadastrar Locação");
+        rentalOptions.add("2 - Listar Todas as Locações");
+        rentalOptions.add("3 - Remover Locação");
+        rentalOptions.add("0 - Voltar ao Anterior");
+
+        var rentalMenu = new Menu(rentalOptions, "Menu Locações");
+
         do {
-            switch (Menu()) {
+            switch (mainMenu.showMenu()) {
                 case 1:
+                    switch (costumerMenu.showMenu()) {
+                        case 1:
+                            //Cadastrar Cliente
+                            costumers.add(CreateCostumer());
+                            break;
+                        case 2:
+                            //Listar Todos
+                            ShowCostumerList(costumers);
+                            break;
+                        case 3:
+                            //Remover Cliente
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Opção Inexiste.");
+                    }
                     break;
                 case 2:
+                    switch (vehicleMenu.showMenu()) {
+                        case 1:
+                            //Cadastrar Veiculo
+                            break;
+                        case 2:
+                            //Listar Todos
+                            break;
+                        case 3:
+                            //Remover Veiculo
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Opção Inexiste.");
+                    }
                     break;
                 case 3:
+                    switch (rentalMenu.showMenu()) {
+                        case 1:
+                            //Cadastrar Locação
+                            break;
+                        case 2:
+                            //Listar Todos
+                            break;
+                        case 3:
+                            //Remover Locação
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Opção Inexiste.");
+                    }
                     break;
                 case 4:
                     System.exit(0);
@@ -31,18 +135,6 @@ public class Main {
                     System.out.println("Opção Inválida!");
                     break;
             }
-        }while (true);
-
-        /*var p1 = new Costumer("123", "Felipe", "0987", "000001");
-        p1.showInfo();
-
-        var v1 = new Car("Volkswagen", "Polo", 2025, 300.00, 5, 4);
-        v1.showInfo();
-
-        var v2 = new Bike("Honda", "CG150", 2025, 100.00, 150, false);
-        v2.showInfo();
-
-        var v3 = new Truck("Scania", "1313", 2025, 800.00, 10, 3);
-        v3.showInfo();*/
+        } while (true);
     }
 }
