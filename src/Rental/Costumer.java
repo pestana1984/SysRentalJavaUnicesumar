1fc94bb242af97a41d486d5990bd22dffdd41d48
felipe.pestana <felipe.pestana@sitic.com.br>
2025-09-17 22:10:11 -0300

    Terceiro dia da revisão

--- src/Rental/Costumer.java
+++ src/Rental/Costumer.java
@@ -1,14 +1,21 @@
 package Rental;
 
+import java.util.Date;
+
 public class Costumer extends Person{
 
     private String creditCardNumber;
     private String licenseNumber;
+    private Date expirationLicense;
 
     public Costumer(String cpf, String name, String creditCardNumber, String licenseNumber) {
         super(cpf, name);
         this.creditCardNumber = creditCardNumber;
         this.licenseNumber = licenseNumber;
+    }
+
+    public Costumer(String cpf, String name){
+        super(cpf, name);
     }
 
     public String getCreditCardNumber() {
